eee2c5ccf93ef078c88e1e9010531f97a73034b8
olgasavchuk <olga.savchuk.pm@gmail.com>
2016-07-12 23:13:09 +0300

    new class Matrix is added

---
+++ src/module1/Matrix.java
@@ -0,0 +1,58 @@
+package module1;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Matrix {
+
+    int n;
+    int m;
+    int[][] matrix;
+
+    final static int multiplier = 10;
+
+    public Matrix() throws Exception {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("n = ");
+        this.n = Integer.parseInt(reader.readLine());
+        System.out.print("m = ");
+        this.m = Integer.parseInt(reader.readLine());
+
+        this.matrix = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                this.matrix[i][j] = (int)(multiplier * Math.random());
+            }
+        }
+    }
+
+    public Matrix(int n, int m) {
+
+        this.n = n;
+        this.m = m;
+
+        this.matrix = new int[n][m];
+    }
+
+    public void showMatrix() {
+
+        for (int i = 0; i < this.matrix.length; i++) {
+            for (int j = 0; j < this.matrix[i].length; j++) {
+                System.out.print(this.matrix[i][j]+ " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public int getElement(int i, int m) {
+
+        return this.matrix[i][m];
+    }
+
+    public void setElement(int i, int j, int value) {
+
+        this.matrix[i][j] = value;
+    }
+}
